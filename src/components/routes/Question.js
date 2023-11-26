5518426a8068a026cc5528a228fbae923f928f3d
IsherfG <isherftheg@gmail.com>
2023-11-26 17:19:19 +0630

    Question Template

--- src/components/routes/Question.js
+++ src/components/routes/Question.js
@@ -5,7 +5,96 @@ import Global from "../styles/global.css";
 function Question() {
   return (
     <div>
-
+      <div>
+        <text>
+          1.I feel a deep sense of wonder and astonishment when I watch skilled
+          stage magicians perform.
+        </text>
+      </div>
+      <div>
+        <text>
+          2. The complexity and mystery of stage magic performances often leave
+          me in awe.
+        </text>
+      </div>
+      <div>
+        <text>
+          3. Witnessing certain stage magic tricks gives me a sense of amazement
+          and reverence.
+        </text>
+      </div>
+      <div>
+        <text>
+          4. I experience a profound sense of wonderment when I'm deceived by a
+          well-executed magic trick.
+        </text>
+      </div>
+      <div>
+        <text>
+          5. I am often struck by the sheer brilliance and artistry behind stage
+          magic illusions.
+        </text>
+      </div>
+      <div>
+        <text>
+          6. Witnessing stage magic performances leaves me feeling captivated
+          and entranced.
+        </text>
+      </div>
+      <div>
+        <text>
+          7. The intricacy and precision of stage magic illusions often leave me
+          in a state of awe.
+        </text>
+      </div>
+      <div>
+        <text>
+          8. I am amazed by the ability of stage magicians to manipulate my
+          perceptions and senses.
+        </text>
+      </div>
+      <div>
+        <text>
+          9. Certain stage magic tricks evoke a sense of astonishment that is
+          hard to put into words.
+        </text>
+      </div>
+      <div>
+        <text>
+          10. I find myself marveling at the ingenuity and creativity behind
+          well-executed magic performances.
+        </text>
+      </div>
+      <div>
+        <text>
+          11. The surprises and illusions presented in stage magic performances
+          often leave me speechless.
+        </text>
+      </div>
+      <div>
+        <text>
+          12. I feel a sense of enchantment and fascination when I'm engaged
+          with a stage magic trick.
+        </text>
+      </div>
+      <div>
+        <text>
+          13. Witnessing stage magic performances can be a source of profound
+          and uplifting experiences.
+        </text>
+      </div>
+      <div>
+        <text>
+          14. The mastery of stage magicians in creating illusions fills me with
+          wonder and admiration.
+        </text>
+      </div>
+      <div>
+        <text>
+          15. I find myself entranced by the spectacle and spectacle of
+          well-performed stage magic.
+        </text>
+      </div>
     </div>
   );
 }
