41e3b83b3128e34aa68a29e096a1e3f73469815b
IsherfG <isherftheg@gmail.com>
2023-12-17 05:32:15 +0630

    Added 6 more languages

--- src/components/routes/Form.js
+++ src/components/routes/Form.js
@@ -13,30 +13,112 @@ function Home() {
   };
 
   return (
-    <div className="container">
-      <div className="Flag">
-        {" "}
-        <Link to="/Home">
-          <Flag
-            country="US"
-            role="button"
-            size={100}
-            onClick={() => changeLanguage("en")}
-          />
-        </Link>
+    <div>
+      <div className="container">
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="US"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("en")}
+            />
+          </Link>
+        </div>
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="NL"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("nl")}
+            />
+          </Link>
+        </div>
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="TH"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("th")}
+            />
+          </Link>
+        </div>
       </div>
-      <div className="Flag">
-        {" "}
-        <Link to="/Home">
-          <Flag
-            country="NL"
-            role="button"
-            size={100}
-            onClick={() => changeLanguage("nl")}
-          />
-        </Link>
+      <div className="container">
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="ES"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("es")}
+            />
+          </Link>
+        </div>
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="IT"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("it")}
+            />
+          </Link>
+        </div>
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="DK"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("da")}
+            />
+          </Link>
+        </div>
       </div>
-      {/* Add more buttons for other languages if needed */}
+      <div className="container">
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="PT"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("pt")}
+            />
+          </Link>
+        </div>
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="FI"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("fi")}
+            />
+          </Link>
+        </div>
+        <div className="Flag">
+          {" "}
+          <Link to="/Home">
+            <Flag
+              country="PL"
+              role="button"
+              size={100}
+              onClick={() => changeLanguage("pl")}
+            />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
